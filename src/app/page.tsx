846ca4699fdf1b021d06fc522cde88ab88d3a7e1
PabloMur <polillomurillo@gmail.com>
2024-03-12 17:12:46 -0300

    a

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <p>
         Una app para que hagamos una lista de lugares que nos gustaria ir, y asi
         cuando no sabes que hacer pordes fijarte que onda por acá
